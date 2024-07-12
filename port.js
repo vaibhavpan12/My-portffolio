bec944dcd5cdac12b5386be4545c3c97c1622456
vaibhavpan12 <vaibhavpan7558@gmail.com>
2024-07-12 17:51:10 +0530

    Update port.js

--- port.js
+++ port.js
@@ -16,55 +16,27 @@ function mouse3deffect() {
 }
 mouse3deffect()
 
-function smoothscroll(){
-    function smootscrolling(){}
-const lenis = new Lenis()
+function smoothscroll() {
+    function smootscrolling() { }
+    const lenis = new Lenis()
 
-lenis.on('scroll', (e) => {
-    console.log(e)
-})
+    lenis.on('scroll', (e) => {
+        console.log(e)
+    })
 
-function raf(time) {
-    lenis.raf(time)
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
     requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
 }
 smoothscroll()
 
-function scroll() {
 
-    let tl = gsap.timeline({
-        // yes, we can add it to an entire timeline!
-        scrollTrigger: {
-            trigger: '.page2',
-            // pin: true, // pin the trigger element while active
-            start: '0% 50%', // when the top of the trigger hits the top of the viewport
-            end: '50% 50%', // end after scrolling 500px beyond the start
-            scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollba
-            // markers: true
-
-        }
-    });
-    tl.to("#myimg", {
-        bottom: "-300px",
-        width: "150px",
-        left: 0,
-
-    }, 0)
-    tl.to(".imagbg", {
-        scale: 1
-    }, 0)
-    tl.to("#myimg", {
-        opacity: 0
-    }, 0)
-
-}
-scroll()
 
 function skillanimaiton() {
-    let t2 = gsap.timeline({
+    let tl1 = gsap.timeline({
         scrollTrigger: {
             trigger: '.main-section',
             pin: true,
@@ -75,7 +47,7 @@ function skillanimaiton() {
         }
     });
 
-    t2.to("#animation", {
+    tl1.to("#animation", {
         opacity: 1,
         filter: "blur(0px)"
     })
@@ -143,11 +115,6 @@ function skillanimaiton() {
             filter: "blur(0px)",
             delay: -0.3
         })
-    // .to(".page3", {
-    //     transform: 'translateY(-900px)',
-    //     // top:"-500px",
-    //     // delay:-0.1
-    // })
 }
 
 skillanimaiton()
@@ -166,53 +133,6 @@ function nameanimation() {
     }, 0)
 }
 nameanimation()
-
-
-function page3animation() {
-    let t3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.page3',
-            // pin: true,
-            start: '0% 50%',
-            end: '50% 50%',
-            scrub: true,
-            // markers: true,
-        }
-    });
-
-    t3.from(".page3 h1", {
-        y: 50,
-        opacity: 0,
-
-    })
-    t3.from(".starbucksimage", {
-        y: 50,
-        opacity: 0,
-
-    })
-    t3.from(".swtimage", {
-        y: 50,
-        opacity: 0,
-
-    })
-    t3.from(".flaashnetimage", {
-        y: 50,
-        opacity: 0,
-
-    })
-    t3.from(".geminiimage", {
-        y: 50,
-        opacity: 0,
-
-    })
-    t3.from(".shoppersimage", {
-        y: 50,
-        opacity: 0,
-
-    })
-}
-
-page3animation()
 
 
 
@@ -237,55 +157,6 @@ function mousemove() {
 }
 mousemove()
 
-
-
-function footeranimaiton() {
-    let t4 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.page4',
-            // pin: true,
-            start: '5% 50%',
-            end: '45% 50%',
-            scrub: 1,
-            // markers: true,
-        }
-    });
-
-    t4.from(".mail", {
-        y: 50,
-        opacity: 0
-    })
-    t4.from(".navi a", {
-        y: 50,
-        opacity: 0
-    })
-    t4.from(".contact-form input ", {
-        y: 50,
-        opacity: 0
-    })
-    t4.from(".form-group textarea ", {
-        y: 50,
-        opacity: 0
-    })
-    // t4.from(".form-group button",{
-    //     y:50,
-    //     opacity:0
-    // })
-    // t4.from(".but a",{
-    //     y:50,
-    //     opacity:0
-    // })
-    t4.from(".navcon a", {
-        y: 50,
-        opacity: 0
-    })
-    t4.from("#copyright", {
-        y: 50,
-        opacity: 0
-    })
-}
-footeranimaiton()
-
 function navbaranimaiton() {
     navbar = document.querySelector(".menubar img")
     wrong = document.querySelector(".wrong")
@@ -307,33 +178,39 @@ navbaranimaiton()
 
 function bgrotaion() {
     let t1 = gsap.timeline({
-        // yes, we can add it to an entire timeline!
         scrollTrigger: {
             trigger: '.page2',
-            // pin: true, // pin the trigger element while active
-            start: '0% 50%', // when the top of the trigger hits the top of the viewport
-            end: '50% 50%', // end after scrolling 500px beyond the start
-            scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollba
+            start: '0% 50%',
+            end: '50% 50%',
+            scrub: true,
             // markers: true
 
         }
     });
     t1.to(".bg", {
-        //   transform: 'rotate(100deg)'
-        // top:'900px',
         rotate: 180
 
     })
+    t1.to("#myimg", {
+        bottom: "-300px",
+        width: "150px",
+        left: 0,
+
+    }, 0)
+    t1.to(".imagbg", {
+        scale: 1
+    }, 0)
+    t1.to("#myimg", {
+        opacity: 0
+    }, 0)
 
 
     let t2 = gsap.timeline({
-        // yes, we can add it to an entire timeline!
         scrollTrigger: {
             trigger: '.page3',
-            // pin: true, // pin the trigger element while active
-            start: '0% 50%', // when the top of the trigger hits the top of the viewport
-            end: '50% 50%', // end after scrolling 500px beyond the start
-            scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollba
+            start: '0% 50%',
+            end: '50% 50%',
+            scrub: true,
             // markers: true
 
         }
@@ -343,7 +220,36 @@ function bgrotaion() {
         rotate: 360
 
     })
+    t2.from(".page3 h1", {
+        y: 50,
+        opacity: 0,
 
+    })
+    t2.from(".starbucksimage", {
+        y: 50,
+        opacity: 0,
+
+    })
+    t2.from(".swtimage", {
+        y: 50,
+        opacity: 0,
+
+    })
+    t2.from(".flaashnetimage", {
+        y: 50,
+        opacity: 0,
+
+    })
+    t2.from(".geminiimage", {
+        y: 50,
+        opacity: 0,
+
+    })
+    t2.from(".shoppersimage", {
+        y: 50,
+        opacity: 0,
+
+    })
 
     let t3 = gsap.timeline({
 
@@ -359,6 +265,30 @@ function bgrotaion() {
     t3.to(".bg", {
         rotate: 530
 
+    })
+    t3.from(".mail", {
+        y: 50,
+        opacity: 0
+    })
+    t3.from(".navi a", {
+        y: 50,
+        opacity: 0
+    })
+    t3.from(".contact-form input ", {
+        y: 50,
+        opacity: 0
+    })
+    t3.from(".form-group textarea ", {
+        y: 50,
+        opacity: 0
+    })
+    t3.from(".navcon a", {
+        y: 50,
+        opacity: 0
+    })
+    t3.from("#copyright", {
+        y: 50,
+        opacity: 0
     })
 }
 bgrotaion()
